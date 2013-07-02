190cbbe0126cfbe8b72fb507c929995838aff4ff
riedingerb <brianriedinger@Macbook.local>
2013-07-01 21:22:30 -0600

    rename contact to locations

--- routes/contact.js
+++
@@ -1,8 +0,0 @@
-// Route responsible for main page
-module.exports = function(app) {
-  app.get('/contact', function (req, res){
-    console.log("Contact page requested");
-
-    res.render('contact');
-  });
-}
